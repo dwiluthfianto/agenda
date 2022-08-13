c0050379c9df85b3ea8b2bf333e78e58a93e4a70
Dwiluthfianto <dwiluthfianto72@gmail.com>
2022-08-13 16:44:25 +0700

    react-tailwind

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ function App() {
     <div className="App">
       <p>Miscellaneous installed</p>
       <ul>
+        <li>tailwindcss</li>
         <li>react-router-dom</li>
         <li>react-app-rewired</li>
         <li>@reduxjs/toolkit</li>
